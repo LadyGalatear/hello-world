7dbae9ac5a5f69d0b202ccf22efa9eddb56191d3
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-01-20 13:57:29 -0800

    Add app.listen to app.js, change type in package.json

--- app.js
+++ app.js
@@ -4,7 +4,17 @@ import express from 'express';
 // Port number definition
 const PORT = 3000;
 
+// Create Express app
+const app = express();
+
 // Root path definition
 app.get('/', (req, res) => {
+    // Send message to client
     res.send('Hello, World!');
+})
+
+// Start server
+app.listen(PORT, () => {
+    console.log(`Server is running at
+        http://localhost:${PORT}`);
 })
