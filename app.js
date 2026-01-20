93d3c631f361585ce345be0d93d8de50a56edc62
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-01-20 13:45:00 -0800

    Add app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+// Express module import
+import express from 'express';
+
+// Port number definition
+const PORT = 3000;
+
+// Root path definition
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+})
